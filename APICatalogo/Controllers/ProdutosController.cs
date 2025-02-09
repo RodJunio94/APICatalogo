71afde0564f4d867a979b2ce465c6fcea8274a4f
JunioRodrigues <junio@buildbox.com.br>
2025-02-09 00:14:21 -0300

    foi realizado o get de produto

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,40 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+
+        if(produtos is null)
+            return NotFound();
+
+        return produtos;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Produto> GetById(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
+
+        if (produto is null)
+            return NotFound("Produto não encontrado");
+
+        return produto;
+    }
+}
